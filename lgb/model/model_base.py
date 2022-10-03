5bec6d3516569b914ffef9e89512599ee09bf595
Chung-Yi <wty81213@hotmail.com>
2022-10-03 09:30:37 +0800

    remove lgb

--- lgb/model/model_base.py
+++
@@ -1,7 +0,0 @@
-class Model:
-    def __init__(self, model_name, config):
-        self.model_name = model_name
-        self.config = config
-
-    def train(self):
-        print("training model...")
