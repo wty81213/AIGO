255d8cd268d9c72f9a0cc5449868d53d5c3df473
Chung-Yi <wty81213@hotmail.com>
2022-10-03 09:22:54 +0800

    create model

---
+++ model/xgb/model/model.py
@@ -0,0 +1,8 @@
+from pytorch_tabnet.tab_model import TabNetRegressor,TabNetClassifier
+
+
+def tabnetregressor_model():
+    return TabNetRegressor
+
+def tabNetclassifier_model():
+    return TabNetClassifier    
