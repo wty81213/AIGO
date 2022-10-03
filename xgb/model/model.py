0457611ba4ee95ae4f92543838265510b4553750
Chung-Yi <wty81213@hotmail.com>
2022-10-03 09:21:57 +0800

    remove xgb folder

--- xgb/model/model.py
+++
@@ -1,8 +0,0 @@
-from pytorch_tabnet.tab_model import TabNetRegressor,TabNetClassifier
-
-
-def tabnetregressor_model():
-    return TabNetRegressor
-
-def tabNetclassifier_model():
-    return TabNetClassifier    
--- xgb/model/test.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd 
-data = pd.read_parquet('./data_feature/F_base_cloud_03.parquet', engine='fastparquet')
-
-output = pd.DataFrame(data.dtypes).reset_index().rename(columns = {0:'datatype'})
-
